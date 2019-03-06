80c9afdd6fec65b0f9114ed0e40551d87a674854
Brendan Prinz <brendanprinz@gmail.com>
2019-03-05 22:35:34 -0800

    boiler plate

--- src/app.jsx
+++ src/app.jsx
@@ -12,11 +12,32 @@ class App extends Component {
     };
   }
 
-
   render() {
     return (
       <div className="App">
-        <h1>Hi</h1>
+
+        {/* <div className='section1'>
+          <h1>hi</h1>
+        </div> */}
+
+        {/* <div>
+          <h1>hi</h1>
+        </div>
+
+        <div className='section2'>
+          <h1>hi</h1>
+        </div> */}
+
+{/* <div className="parallax"></div>
+
+<div className='redDiv'>
+Scroll Up and Down this page to see the parallax scrolling effect.
+This div is just here to enable scrolling.
+Tip: Try to remove the background-attachment property to remove the scrolling effect.
+</div>
+
+<div className="parallax"></div> */}
+
       </div>
     );
   }
